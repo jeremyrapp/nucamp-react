1ed868254e2c43deb71a81001a7f75deb9a261fd
Jeremy Rapp <rappj45@gmail.com>
2020-04-07 03:44:52 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -38,7 +38,7 @@ function RenderCampsite({ campsite }) {
   return <div />;
 }
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, campsiteId}) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -58,7 +58,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
-        <CommentForm></CommentForm>
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -83,7 +83,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments 
+                comments={props.comments}
+                addComment={props.addComment}
+                campsiteId={props.campsite.id}
+            />
         </div>
       </div>
     );
@@ -103,9 +107,10 @@ class CommentForm extends React.Component {
   }
 
   handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(values));
-    alert("Current state is: " + JSON.stringify(values));
+    this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
+   
 
   toggleModal() {
     this.setState({
