7a031759056c53a94d19d658207cdb381dbabe44
Jeremy Rapp <rappj45@gmail.com>
2020-04-08 02:03:58 -0500

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -15,8 +15,9 @@ import {
   Col,
   Label,
 } from "reactstrap";
-import { LocalForm, Control, Actions, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+import { LocalForm, Control, Actions, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -38,7 +39,7 @@ function RenderCampsite({ campsite }) {
   return <div />;
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -66,6 +67,26 @@ function RenderComments({comments, addComment, campsiteId}) {
 }
 
 function CampsiteInfo(props) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+  if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
   if (props.campsite) {
     return (
       <div className="container">
@@ -83,11 +104,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments 
-                comments={props.comments}
-                addComment={props.addComment}
-                campsiteId={props.campsite.id}
-            />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.id}
+          />
         </div>
       </div>
     );
@@ -108,9 +129,13 @@ class CommentForm extends React.Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.addComment(
+      this.props.campsiteId,
+      values.rating,
+      values.author,
+      values.text
+    );
   }
-   
 
   toggleModal() {
     this.setState({
@@ -127,25 +152,29 @@ class CommentForm extends React.Component {
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
           <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
           <ModalBody>
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}  initialState={{ rating: '1', author:'', text:'' }}>
+            <LocalForm
+              onSubmit={(values) => this.handleSubmit(values)}
+              initialState={{ rating: "1", author: "", text: "" }}
+            >
               <div className="form-group">
                 <Row>
                   <Label htmlFor="rating" md={12}>
                     Rating
                   </Label>
                   <Col md={12}>
-                      <Control.select
+                    <Control.select
                       model=".rating"
                       id="rating"
                       name="rating"
-                      className="form-control">
+                      className="form-control"
+                    >
                       <option>1</option>
                       <option>2</option>
                       <option>3</option>
                       <option>4</option>
                       <option>5</option>
-                      </Control.select>
-                    </Col>
+                    </Control.select>
+                  </Col>
                 </Row>
               </div>
               <div className="form-group">
@@ -198,10 +227,10 @@ class CommentForm extends React.Component {
               </div>
               <div className="form-group">
                 <Row>
-                <Col md={12}>
-                  <Button type="submit" color="primary">
-                    Submit
-                  </Button>
+                  <Col md={12}>
+                    <Button type="submit" color="primary">
+                      Submit
+                    </Button>
                   </Col>
                 </Row>
               </div>
