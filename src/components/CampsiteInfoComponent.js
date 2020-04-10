4eb4b1b097520a94854063ceb91326c8a1cd97d6
Jeremy Rapp <rappj45@gmail.com>
2020-04-10 03:35:30 -0500

    Fetch from Server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,23 +1,10 @@
 import React, { Component } from "react";
-import {
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  Breadcrumb,
-  BreadcrumbItem,
-  Row,
-  Col,
-  Label,
-} from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, 
+  Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from "reactstrap";
 import { Link } from "react-router-dom";
 import { LocalForm, Control, Actions, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -28,7 +15,7 @@ function RenderCampsite({ campsite }) {
     return (
       <div className="col-md-5 m-1">
         <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
           <CardBody>
             <CardText>{campsite.description}</CardText>
           </CardBody>
